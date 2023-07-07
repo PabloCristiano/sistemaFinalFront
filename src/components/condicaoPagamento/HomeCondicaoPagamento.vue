84a851f396452c8ad163a8905642466b7d4ddf19
PabloCristiano <48129244+PabloCristiano@users.noreply.github.com>
2023-07-06 21:11:13 -0300

    up

---
+++ src/components/condicaoPagamento/HomeCondicaoPagamento.vue
@@ -0,0 +1,217 @@
+<template>
+  <div class="container" style="margin-bottom: -90px">
+    <b-overlay :show="isLoading" rounded="sm">
+      <div class="card mb-5">
+        <div class="card-header">Cadastrar Condição de Pagamento</div>
+        <div class="card-body">
+          <div class="">
+            <vue-good-table
+              :columns="columns"
+              :rows="condicaoPagamentos"
+              @on-cell-click="selectCellCondidaoPagamento"
+              :search-options="{
+                enabled: true,
+                placeholder: 'Procure por uma Condição de Pagamento',
+                skipDiacritics: true,
+              }"
+              :pagination-options="{
+                enabled: true,
+                mode: 'records',
+                perPage: 5,
+                position: 'end',
+                perPageDropdown: [5, 10],
+                prevLabel: 'Anterior',
+                nextLabel: 'Proximo',
+                rowsPerPageLabel: 'Qtd por página',
+                ofLabel: 'de',
+                pageLabel: 'Pagina', // for 'pages' mode
+              }"
+            >
+              <template slot="table-row" slot-scope="props">
+                <span v-if="props.column.field === 'btn'">
+                  <a
+                    @click="showModalAlterarCondicaoPagamento(props.row.id)"
+                    size="sm"
+                    class="btn btn-sm me-1 mb-1"
+                    data-backdrop="static"
+                    title="EDITAR"
+                    style="background-color: #f0f8ff"
+                  >
+                    <i class="bx bx-edit-alt"></i>
+                  </a>
+                  <a
+                    @click="showModalExcluirCondicaoPagamento(props.row.id)"
+                    size="sm"
+                    class="btn btn-sm me-1 mb-1"
+                    data-backdrop="static"
+                    title="EXCLUIR"
+                    style="background-color: #f0f8ff"
+                  >
+                    <i class="bx bx-trash-alt"></i>
+                  </a>
+                </span>
+              </template>
+            </vue-good-table>
+          </div>
+        </div>
+        <div class="card-footer">
+          <div class="col-md-12 text-end">
+            <div class="col-md-2 offset-md-10">
+              <button
+                class="btn btn-sm btn-dark"
+                @click="ShowModalCondicaoPagamento()"
+              >
+                <i class="bx bx-plus-circle"></i> Adicionar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </b-overlay>
+    <br /><br />
+    <Modal
+      :formulario="form_CondicaoPagamento"
+      :funcOnReset="onReset"
+      :funcgetListCondicaoPagamento="getListCondicaoPagamento"
+    ></Modal>
+  </div>
+</template>
+<script>
+import Modal from "./Modal.vue";
+import { VueGoodTable } from "vue-good-table";
+export default {
+  components: { VueGoodTable, Modal },
+  data() {
+    return {
+      modal_form_condicaoPagamento: "modal_form_condicaoPagamento",
+      isLoading: false,
+      condicaoPagamentos: [],
+      columns: [
+        {
+          label: "ID",
+          field: "id",
+          thClass: "text-center",
+          tdClass: "text-center",
+        },
+        {
+          label: "Condição de Pagamento",
+          field: "condicao_pagamento",
+          thClass: "text-center",
+          tdClass: "text-center",
+        },
+        {
+          label: "Juros",
+          field: "juros",
+          thClass: "text-center",
+          tdClass: "text-center",
+        },
+        {
+          label: "Multa",
+          field: "multa",
+          thClass: "text-center",
+          tdClass: "text-center",
+        },
+        {
+          label: "Desconto",
+          field: "desconto",
+          thClass: "text-center",
+          tdClass: "text-center",
+        },
+        {
+          label: "Ações",
+          sortable: false,
+          field: "btn",
+          html: true,
+          thClass: "text-center",
+          tdClass: "text-center",
+        },
+      ],
+      form_CondicaoPagamento: {
+        id: "",
+        condicaoPagamento: "",
+        juros: 0,
+        multa: 0,
+        desconta: 0,
+        data_create: "",
+        data_alt: "",
+        titulo: "",
+        btn: "",
+        disabled: false,
+      },
+    };
+  },
+  created() {
+    this.getListCondicaoPagamento();
+  },
+  methods: {
+    selectCellCondidaoPagamento(params) {
+      if (this.functionEstado) {
+        this.functionEstado(params);
+      }
+    },
+    getListCondicaoPagamento() {
+      //   this.isLoading = true;
+      //   ServiceFormaPagamento.getAll()
+      //     .then((obj) => {
+      //       if (obj) {
+      //         this.formaPagamentos = obj;
+      //       }
+      //       this.isLoading = false;
+      //     })
+      //     .catch((error) => {
+      //       console.log(error);
+      //     });
+    },
+    ShowModalCondicaoPagamento() {
+      this.onReset();
+      this.form_CondicaoPagamento.titulo = "Cadastrar Condição de Pagamento";
+      this.form_CondicaoPagamento.btn = "Salvar";
+      this.$bvModal.show(this.modal_form_condicaoPagamento);
+    },
+    showModalAlterarFormaPagamento(id) {
+      console.log(id);
+      //   this.onReset();
+      //   var titulo = "Alterar Condição de Pagamento";
+      //   var btn = "Alterar";
+      //   var disabled = false;
+      //   this.funcGetById(id, titulo, btn, disabled);
+    },
+    showModalExcluirFormaPagamento(id) {
+      console.log(id);
+      //   this.onReset();
+      //   var titulo = "Excluir Condição de Pagamento";
+      //   var btn = "Excluir";
+      //   var disabled = true;
+      //   this.funcGetById(id, titulo, btn, disabled);
+    },
+    onReset() {
+      this.form_CondicaoPagamento.id = "";
+      this.form_CondicaoPagamento.condicaoPagamento = "";
+      this.form_CondicaoPagamento.data_create = "";
+      this.form_CondicaoPagamento.data_alt = "";
+      this.form_CondicaoPagamento.disabled = false;
+    },
+    // funcGetById(id, titulo, btn, disabled) {
+    //   this.isLoading = true;
+    //   ServiceFormaPagamento.getById(id)
+    //     .then((obj) => {
+    //       this.form_CondicaoPagamento.id = obj.data[0].id;
+    //       this.form_CondicaoPagamento.forma_pg = obj.data[0].forma_pg;
+    //       this.form_CondicaoPagamento.data_create = obj.data[0].data_create;
+    //       this.form_CondicaoPagamento.data_alt = obj.data[0].data_alt;
+    //       this.form_CondicaoPagamento.titulo = titulo;
+    //       this.form_CondicaoPagamento.btn = btn;
+    //       this.form_CondicaoPagamento.disabled = disabled;
+    //       this.isLoading = false;
+    //       this.$bvModal.show(this.modal_form_condicaoPagamento);
+    //       return;
+    //     })
+    //     .catch((error) => {
+    //       console.log(error);
+    //     });
+    // },
+  },
+};
+</script>
+<style>
+</style>
---
+++ src/components/condicaoPagamento/Modal.vue
@@ -0,0 +1,233 @@
+<template>
+  <div>
+    <b-modal
+      :id="modal_form_condicaoPagamento"
+      size="lg"
+      :header-bg-variant="headerBgVariant"
+      :header-text-variant="headerTextVariant"
+      no-close-on-backdrop
+      hide-footer
+    >
+      <template v-slot:modal-header>
+        <h5>{{ form.titulo }}</h5>
+        <b-button
+          style="border: 0"
+          size="sm"
+          variant="outline-light"
+          @click="closeCondicaoPagamento()"
+        >
+          X
+        </b-button>
+      </template>
+      <b-card
+        class="mt-0 mb-3"
+        header="Formulário de cadastro Forma de Pagamento"
+      >
+        <slot name="conteudo">
+          <b-form>
+            <div class="row col-12">
+              <div class="col-md-2">
+                <label>Código:</label>
+                <b-form-input
+                  id="id"
+                  type="text"
+                  placeholder="Id"
+                  v-model="form.id"
+                  disabled
+                >
+                </b-form-input>
+              </div>
+              <div class="col-md-10">
+                <label
+                  >Condição de Pagamento:<b style="color: rgb(245, 153, 153)">
+                    *</b
+                  ></label
+                >
+                <b-form-input
+                  id="servico"
+                  type="text"
+                  placeholder="Condição de Pagamento"
+                  
+                  v-model="form.condicaoPagamento"
+                  :disabled="form.disabled"
+                >
+                </b-form-input>
+                <small style="font-size: 11px; color: red">
+                  <!-- {{ validationMsg($v.form.condicaoPagamento) }} -->
+                </small>
+              </div>
+            </div>
+            <div>
+              <small class="mt-2" style="font-size: 12px"
+                >Campos com <b style="color: rgb(245, 153, 153)">*</b> são
+                obrigatórios !</small
+              >
+            </div>
+          </b-form>
+        </slot>
+        <slot name="botao">
+          <div class="d-flex justify-content-end">
+            <b-button
+              class="btn btn-sm me-1"
+              type="button"
+              variant="dark"
+              @click.prevent="closeCondicaoPagamento()"
+            >
+              Cancelar
+            </b-button>
+            <b-button
+              class="btn btn-sm me-1"
+              type="button"
+              variant="dark"
+              @click.prevent="onSubmit()"
+            >
+              {{ form.btn }}<i class="bx bx-check"></i>
+            </b-button>
+          </div>
+        </slot>
+        <slot name="rodape">
+          <div class="col-12">
+            <small class="col-6 me-1" style="font-size: 11px"
+              ><b>Data Criação:</b>
+              {{ form.data_create | formataDataTempo }}</small
+            >
+            <small class="col-6 me-1" style="font-size: 11px"
+              ><b>Data Ult-Alteração:</b>
+              {{ form.data_alt | formataDataTempo }}</small
+            >
+          </div>
+        </slot>
+      </b-card>
+    </b-modal>
+  </div>
+</template>
+<script>
+import * as validators from "vuelidate/lib/validators";
+import { validationMessage } from "vuelidate-messages";
+// import { ServiceFormaPagamento } from "../../services/serviceFormaPagamento.js";
+import { formataDataTempo } from "../../rules/filters";
+const formMessages = {
+  required: () => "Campo Obrigatório",
+  txtMinLen: ({ $params }) =>
+    `Campo minimo ${$params.txtMinLen.min} characters.`,
+  txtMaxLen: ({ $params }) =>
+    `Campo maximo ${$params.txtMaxLen.max} characters.`,
+  integer: () => "Campo deve ser um Numero inteiro",
+};
+export default {
+  props: {
+    formulario: { type: Object },
+    funcOnReset: { type: Function },
+    funcgetListCondicaoPagamento: { type: Function },
+  },
+  data() {
+    return {
+      form: this.formulario,
+      headerBgVariant: "dark",
+      headerTextVariant: "light",
+      modal_form_condicaoPagamento: "modal_form_condicaoPagamento",
+    };
+  },
+  filters: {
+    formataDataTempo,
+  },
+  validation: {
+    form: {
+      condicaoPagamento: {
+        required: validators.required,
+        txtMinLen: validators.minLength(3),
+      },
+    },
+  },
+  methods: {
+    validationMsg: validationMessage(formMessages),
+    function_getListFormaPagamento() {
+      this.funcgetListFormaPagamento();
+    },
+    onReset() {
+      this.$v.$reset();
+      this.funcOnReset();
+    },
+    closeCondicaoPagamento() {
+      this.onReset();
+      this.$bvModal.hide(this.modal_form_condicaoPagamento);
+    },
+    onSubmit() {
+      //const vm = this;
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+      } else {
+        if (this.form.btn === "Salvar") {
+          console.log(this.form);
+          //   ServiceFormaPagamento.storeFormaPagamento(this.form)
+          //     .then((response) => {
+          //       if (response.status === 200) {
+          //         notyf.success(response.data.success);
+          //         vm.onReset();
+          //         vm.$bvModal.hide(vm.modal_form_formaPagamento);
+          //         this.function_getListFormaPagamento();
+          //       } else {
+          //         if (response.response.data.errors != null) {
+          //           Object.keys(response.response.data.errors).forEach(function (
+          //             key
+          //           ) {
+          //             notyf.error(response.response.data.errors[key][0]);
+          //           });
+          //         }
+          //       }
+          //     })
+          //     .catch((error) => {
+          //       console.log(error);
+          //     });
+        }
+        if (this.form.btn === "Alterar") {
+          console.log(this.form);
+          //   ServiceFormaPagamento.alterarFormaPagamento(this.form)
+          //     .then((response) => {
+          //       if (response.status === 200) {
+          //         notyf.success(response.data.success);
+          //         vm.onReset();
+          //         vm.$bvModal.hide(vm.modal_form_formaPagamento);
+          //         this.function_getListFormaPagamento();
+          //       } else {
+          //         if (response.response.data.errors != null) {
+          //           Object.keys(response.response.data.errors).forEach(function (
+          //             key
+          //           ) {
+          //             notyf.error(response.response.data.errors[key][0]);
+          //           });
+          //         }
+          //       }
+          //     })
+          //     .catch((error) => {
+          //       console.log(error);
+          //     });
+        }
+        if (this.form.btn === "Excluir") {
+          //   ServiceFormaPagamento.excluirFormaPagamento(this.form.id)
+          //     .then((obj) => {
+          //       if (obj.status === 200) {
+          //         notyf.success(obj.data.success);
+          //         this.onReset();
+          //         this.$bvModal.hide(this.modal_form_formaPagamento);
+          //         this.function_getListFormaPagamento();
+          //       } else {
+          //         if (obj.response.data.erro.length > 0) {
+          //           notyf.error(obj.response.data.erro[0]);
+          //         }
+          //       }
+          //     })
+          //     .catch((error) => {
+          //       console.log(error);
+          //     });
+        }
+      }
+    },
+  },
+};
+</script>
+<style>
+.fail-error {
+  border: 1px solid red;
+}
+</style>

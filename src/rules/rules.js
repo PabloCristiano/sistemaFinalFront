d5db459ab67dea2b98a40340dcb7f64ef0b4040b
PabloCristiano <pcristiano25@gmail.com>
2023-08-26 11:21:52 -0300

    up

--- src/rules/rules.js
+++ src/rules/rules.js
@@ -12,13 +12,13 @@ const Rules = {
       resto;
     for (let i = 1; i <= 9; i++)
       soma = soma + parseInt(cpf.substring(i - 1, i)) * (11 - i);
-    resto = soma * 10 % 11;
+    resto = (soma * 10) % 11;
     if (resto == 10 || resto == 11) resto = 0;
     if (resto != parseInt(cpf.substring(9, 10))) return false;
     soma = 0;
     for (let i = 1; i <= 10; i++)
       soma = soma + parseInt(cpf.substring(i - 1, i)) * (12 - i);
-    resto = soma * 10 % 11;
+    resto = (soma * 10) % 11;
     if (resto == 10 || resto == 11) resto = 0;
     if (resto != parseInt(cpf.substring(10, 11))) return false;
     return true;
@@ -31,7 +31,7 @@ const Rules = {
       d = cnpj.substring(t),
       d1 = parseInt(d.charAt(0)),
       d2 = parseInt(d.charAt(1)),
-      calc = x => {
+      calc = (x) => {
         let n = cnpj.substring(0, x),
           y = x - 7,
           s = 0,
@@ -40,7 +40,7 @@ const Rules = {
           s += n.charAt(x - i) * y--;
           if (y < 2) y = 9;
         }
-        r = 11 - s % 11;
+        r = 11 - (s % 11);
         return r > 9 ? 0 : r;
       };
     return calc(t) === d1 && calc(t + 1) === d2;
@@ -92,23 +92,59 @@ const Rules = {
   },
 
   validardataEmissao(data) {
+    const meses = [
+      "01",
+      "02",
+      "03",
+      "04",
+      "05",
+      "06",
+      "07",
+      "08",
+      "09",
+      "10",
+      "11",
+      "12",
+    ];
+    const dataEmissao = Rules.convertToMilliseconds(data);
+
     const dataAtual = new Date();
-    const dataInserida = new Date(data);
-    if (dataInserida <= dataAtual) {
+    const diaAtual = dataAtual.getDate();
+    const mesAtual = parseInt(meses[dataAtual.getMonth()]);
+    const anoAtual = dataAtual.getFullYear();
+    const dataAtual_convertida = `${anoAtual}-${mesAtual}-${diaAtual}`;
+    const dataAtualV = Rules.convertToMilliseconds(dataAtual_convertida);
+    if (dataEmissao <= dataAtualV) {
       return true; // Data é anterior ou igual à data atual
     } else {
       return false; // Data é posterior à data atual
     }
   },
+
   validarDataEmissao(data, dataEmissao) {
-    const dataInserida = new Date(data);
-    const dataEmissaoDate = new Date(dataEmissao);
+    const dataInserida = Rules.convertToMilliseconds(data);
+    const dataEmissaoDate = Rules.convertToMilliseconds(dataEmissao);
 
     if (dataInserida >= dataEmissaoDate) {
       return true; // Data é posterior ou igual à data de emissão
     } else {
       return false; // Data é anterior à data de emissão
     }
-  }
+  },
+  convertToMilliseconds(dateString) {
+    const dateParts = dateString.split("-");
+    if (dateParts.length !== 3) {
+      throw new Error("Formato de data inválido. Use YYYY-MM-DD.");
+    }
+
+    const year = parseInt(dateParts[0], 10);
+    const month = parseInt(dateParts[1], 10) - 1; // Meses em JavaScript são indexados de 0 a 11
+    const day = parseInt(dateParts[2], 10);
+
+    const dateObject = new Date(year, month, day);
+    const milliseconds = dateObject.getTime();
+
+    return milliseconds;
+  },
 };
 export default Rules;

66dc506c47075d5e8d510c046e1c022fb355f338
PabloCristiano <48129244+PabloCristiano@users.noreply.github.com>
2023-10-24 21:47:30 -0300

    up

--- src/components/contasPagar/HomeContaPagar.vue
+++ src/components/contasPagar/HomeContaPagar.vue
@@ -4,93 +4,141 @@
       <div class="card mb-5">
         <div class="card-header">Contas A Pagar</div>
         <div class="card-body">
-          <div class="row col-md-12 col-sm-12 mb-2 justify-content-start">
-            <div class="col-md-2 col-sm-">
-              <label>Data :</label>
-              <b-form-input
-                id="date"
-                type="date"
-                v-model="form.horario_inicio"
-                ref="horario_inicio"
-                :class="{
-                  'fail-error': false,
-                }"
-              ></b-form-input>
-              <small class="small-msg">
-                <!-- {{ validationMsg($v.form.horario_inicio) }} -->
-              </small>
-            </div>
-            <div class="col-md-2">
-              <label>Código:</label>
-              <b-form-input
-                id="id_profissional"
-                type="number"
-                :class="{
-                  'fail-error': false,
-                }"
-                placeholder="Código"
-                ref="id_profissional"
-                v-model="form.id_profissional"
-              >
-              </b-form-input>
-              <small class="small-msg">
-                <!-- {{ validationMsg($v.form.id_profissional) }} -->
-              </small>
-            </div>
-            <div class="col-md-4">
-              <label
-                >Profissional:<b style="color: rgb(245, 153, 153)"> *</b></label
-              >
-              <b-overlay :show="false" rounded="sm">
-                <b-input-group>
-                  <b-form-input
-                    id="profissional"
-                    type="text"
-                    :class="{
-                      'fail-error': false,
-                    }"
-                    placeholder="Profissional"
-                    v-model="form.profissional"
-                    disabled
-                  >
-                  </b-form-input>
-                  <b-input-group-append>
-                    <b-button
-                      text="Button"
-                      variant="dark"
-                      title="Pesquisar Profissional"
-                      ><svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="16"
-                        height="16"
-                        fill="currentColor"
-                        class="bi bi-search"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
-                        /></svg
-                    ></b-button>
-                  </b-input-group-append>
-                </b-input-group>
+          <div
+            class="row col-md-8 col-sm-8 offset-2 mb-2 justify-content-center align-items-center"
+          >
+            <div class="row">
+              <div class="col-md-6 col-sm-6">
+                <label>Data Emissão:</label>
+                <b-form-input
+                  id="date"
+                  type="date"
+                  v-model="form.horario_inicio"
+                  ref="horario_inicio"
+                  :class="{
+                    'fail-error': false,
+                  }"
+                ></b-form-input>
                 <small class="small-msg">
-                  <!-- {{ validationMsg($v.form.profissional) }} -->
+                  <!-- {{ validationMsg($v.form.horario_inicio) }} -->
                 </small>
-              </b-overlay>
+              </div>
+              <div class="col-md-6 col-sm-6">
+                <label>Data Vencimento:</label>
+                <b-form-input
+                  id="date"
+                  type="date"
+                  v-model="form.horario_inicio"
+                  ref="horario_inicio"
+                  :class="{
+                    'fail-error': false,
+                  }"
+                ></b-form-input>
+                <small class="small-msg">
+                  <!-- {{ validationMsg($v.form.horario_inicio) }} -->
+                </small>
+              </div>
             </div>
-            <div class="col-md-2 mt-4 text-center">
-              <button id="pesquisar" class="btn btn-dark" ref="pesquisar">
-                Pesquisar
-              </button>
+            <div class="row">
+              <div class="col-md-3 col-sm-6">
+                <label>Nº Nota:</label>
+                <b-form-input
+                  id="id_cliente_"
+                  type="number"
+                  :class="{
+                    'fail-error': fasle,
+                  }"
+                  placeholder="Nº Nota"
+                  v-model="form.num_nota"
+                >
+                </b-form-input>
+                <small class="small-msg">
+                  <!-- {{ validationMsg($v.form.id_cliente) }} -->
+                </small>
+              </div>
+              <div class="col-md-3 col-sm-6">
+                <label for="status">Status:</label>
+                <v-select
+                  class="form-select"
+                  :options="statusOptions"
+                  placeholder="Status"
+                  label="status"
+                  v-model="selectedStatus"
+                  style="padding: 2px"
+                ></v-select>
+              </div>
+              <div class="col-md-2">
+                <label>Código:</label>
+                <b-form-input
+                  id="id_profissional"
+                  type="number"
+                  :class="{
+                    'fail-error': false,
+                  }"
+                  placeholder="Código"
+                  ref="id_profissional"
+                  v-model="form.id_profissional"
+                >
+                </b-form-input>
+                <small class="small-msg">
+                  <!-- {{ validationMsg($v.form.id_profissional) }} -->
+                </small>
+              </div>
+              <div class="col-md-4">
+                <label>Fornecedor:</label>
+                <b-overlay :show="false" rounded="sm">
+                  <b-input-group>
+                    <b-form-input
+                      id="profissional"
+                      type="text"
+                      :class="{
+                        'fail-error': false,
+                      }"
+                      placeholder="Profissional"
+                      v-model="form.profissional"
+                      disabled
+                    >
+                    </b-form-input>
+                    <b-input-group-append>
+                      <b-button
+                        text="Button"
+                        variant="dark"
+                        title="Pesquisar Profissional"
+                        ><svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          width="16"
+                          height="16"
+                          fill="currentColor"
+                          class="bi bi-search"
+                          viewBox="0 0 16 16"
+                        >
+                          <path
+                            d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"
+                          /></svg
+                      ></b-button>
+                    </b-input-group-append>
+                  </b-input-group>
+                  <small class="small-msg">
+                    <!-- {{ validationMsg($v.form.profissional) }} -->
+                  </small>
+                </b-overlay>
+              </div>
             </div>
-            <div class="col-md-2 mt-4">
-              <button class="btn btn-dark" title="LIMPAR CAMPOS">
-                <i class="bx bx-trash"></i>
-              </button>
+            <div class="row justify-content-center mt-2">
+              <div class="col-md-2 col-sm-2 mt-2 text-center">
+                <button id="pesquisar" class="btn btn-dark" ref="pesquisar">
+                  Pesquisar
+                </button>
+              </div>
+              <div class="col-md-2 col-sm-2 text-center mt-2">
+                <button class="btn btn-dark" title="LIMPAR CAMPOS">
+                  <i class="bx bx-trash"></i>
+                </button>
+              </div>
             </div>
           </div>
-          <div class="row col-md-12 col-sm-12 mb-4 justify-content-end"></div>
-          <div class="">
+
+          <div class="mt-4">
             <vue-good-table
               :columns="columns"
               :rows="contasPagar"
@@ -154,10 +202,17 @@
 export default {
   data() {
     return {
+      statusOptions: [
+        { status: "PAGO" },
+        { status: "PENDENTE" },
+        { status: "CANCELADA" },
+      ],
+      selectedStatus: null,
       form: {
         horario_inicio: "",
         id_profissional: "",
         profissional: "",
+        num_nota: "",
       },
       contasPagar: [],
       columns: [
